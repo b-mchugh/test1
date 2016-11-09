1615c0687fc8e65c29b2fba72bcc2ab8f9193d83
Brendan Mchugh <bmchugh@redhat.com>
2016-11-09 12:28:01 +0100

    Create myapp.py

---
+++ myapp.py
@@ -0,0 +1,24 @@
+import socket
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostname = socket.gethostname()
+IP = socket.gethostbyname(hostname)
+
+class RH(BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+		message = ['myapp test1' + 'Client: ' + self.address_string() + '<br>',
+					'Server: ' + IP + ' aka ' + hostname + '<br>',
+					'Date: ' + self.date_time_string() + '<br>']
+		for i in message:
+			print(i)
+			self.wfile.write(bytes(i, "utf8"))
+		return
+
+def run():
+	server_address = (IP, 8080)
+	httpd = HTTPServer(server_address, RH)
+	httpd.serve_forever()
+run()
